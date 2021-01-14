d2f6fc097fae957712cc00862153d1e8032e4aad
Ryan Wilson <ryanswilson1@gmail.com>
2021-01-14 12:03:49 -0500

    updated app.js

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Nav from "./components/Nav";
+import EmployeeDirectory from "./pages/EmployeeDirectory";
+import API from "./utils/API";
+import sortFunctions from "./utils/sortFunctions";
 
 function App() {
+  // EmployeesList is the master list, derived from the API.
+  const [employeesList, setEmployeesList] = useState([]);
+  // Employees is the array to be modified with sorting & filtering.
+  const [employees, setEmployees] = useState([]);
+
+  const [searchOptions, setSearchOptions] = useState({
+    key: "firstName",
+    order: "asc",
+    query: "",
+  });
+
+  const onQueryChange = (e) => {
+    const { value } = e.target;
+
+    setSearchOptions((currentSearchOptions) => ({
+      ...currentSearchOptions,
+      query: value,
+    }));
+  };
+
+  const onSortChange = (key, order) => {
+    setSearchOptions((currentSearchOptions) => ({
+      ...currentSearchOptions,
+      key,
+      order,
+    }));
+  };
+
+  // Load all employees and store them with setEmployeesList
+  useEffect(() => {
+    loadEmployeesList();
+  }, []);
+
+  // Only sets employees array once employeesList has changed.
+  useEffect(() => {
+    const modified = employeesList.map((e) => ({
+      ...e,
+      firstName: e.name.first,
+      lastName: e.name.last,
+      fullName: `${e.name.first} ${e.name.last}`,
+    }));
+
+    const filtered = modified.filter((employee) =>
+      employee.fullName
+        .toLowerCase()
+        .includes(searchOptions.query.toLowerCase())
+    );
+
+    filtered.sort(
+      sortFunctions.compareValues(searchOptions.key, searchOptions.order)
+    );
+
+    setEmployees(filtered);
+  }, [employeesList, searchOptions]);
+
+  // Loads all employees and sets them to employeesList
+  async function loadEmployeesList() {
+    try {
+      const response = await API.getUsers();
+      setEmployeesList(response.data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        query={searchOptions.query}
+        onInputChange={onQueryChange}
+        onSortChange={onSortChange}
+        currentKey={searchOptions.key}
+        currentOrder={searchOptions.order}
+      />
+      <EmployeeDirectory employees={employees}></EmployeeDirectory>
     </div>
   );
 }
